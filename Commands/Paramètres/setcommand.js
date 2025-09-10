cae2d4434764fd52e1a4bfaee78805bfedc5fff8
4wip <168364544+4wip@users.noreply.github.com>
2025-09-10 13:36:39 +0200

    Update setcommand.js

--- Commands/Paramètres/setcommand.js
+++ Commands/Paramètres/setcommand.js
@@ -2,6 +2,7 @@ const db = require('../../Events/loadDatabase');
 const fs = require('fs');
 const path = require('path');
 const config = require('../../config.json');
+const { EmbedBuilder } = require('discord.js');
 
 exports.help = {
   name: 'setcommand',
