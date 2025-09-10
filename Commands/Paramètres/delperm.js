2ffdaf1541a1c8c71561da166a06e44ba9c2a519
4wip <168364544+4wip@users.noreply.github.com>
2025-09-10 13:37:54 +0200

    Update delperm.js

--- Commands/Paramètres/delperm.js
+++ Commands/Paramètres/delperm.js
@@ -1,6 +1,6 @@
 const db = require('../../Events/loadDatabase');
 const config = require('../../config.json');
-
+const { EmbedBuilder } = require('discord.js');
 exports.help = {
   name: 'delperm',
   helpname: 'delperm [perms] [role]',
