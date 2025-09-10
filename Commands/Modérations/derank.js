1bb85a2ae403ce86267e7ceebb800d2dfaa76df2
4wip <168364544+4wip@users.noreply.github.com>
2025-09-10 13:39:25 +0200

    Update derank.js

--- Commands/Modérations/derank.js
+++ Commands/Modérations/derank.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const db = require('../../Events/loadDatabase');
 const config = require('../../config.json');
 const sendLog = require('../../Events/sendlog');
